71f8a997fb6695222da7a32de6e57c9a66eeaa75
mdakhib <mohamedakhib@gmail.com>
2020-09-25 11:33:05 +0530

    pages rendered to browser

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ const app = express()
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyparser.urlencoded)({
-    extended: true
-});
+app.use(bodyparser.urlencoded({
+    extended:true
+}))
 
+app.get("/", (req, res) => {
+    res.render("home")
+})
 
+app.get("/login", (req, res) => {
+    res.render("login")
+})
 
-
+app.get("/register", (req, res) => {
+    res.render("register")
+})
 
 
 
